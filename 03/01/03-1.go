3d08c50514b4ff735706fd158b42a9e68814a2e1
Blair Wilcox <reccanti@gmail.com>
2020-12-03 13:17:52 -0500

    Day 3 Problem 1

--- 03/01/03-1.go
+++ 03/01/03-1.go
@@ -3,15 +3,32 @@ package main
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/reccanti/a-soon-to-be-abandoned-advent-of-code-repo/util"
 )
 
 func main() {
+	// get the "hill" data
 	filename := os.Args[1]
 	input, err := util.ParseRelativeFile(filename)
 	if err != nil {
 		return
 	}
-	fmt.Println(*input)
+	hill := *input
+
+	// scan the hill
+	rows := strings.Split(hill, "\n")
+	xPos := 0
+	treeCount := 0
+	for _, row := range rows {
+		// determine if there is a tree at the current position
+		char := string(row[xPos])
+		if char == "#" {
+			treeCount += 1
+		}
+		// increment the x position
+		xPos = (xPos + 3) % len(row)
+	}
+	fmt.Println(treeCount)
 }
