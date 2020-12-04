eae81c14b8f543ebfebff272354bae5dfb59ff70
Blair Wilcox <reccanti@gmail.com>
2020-12-04 01:11:28 -0500

    Day 4 Part 1

---
+++ 04/01/04-1.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strings"
+
+	"github.com/reccanti/a-soon-to-be-abandoned-advent-of-code-repo/util"
+)
+
+// ([^\s]*)\s?
+
+type Passport = map[string]string
+
+func main() {
+	// get the "hill" data
+	filename := os.Args[1]
+	input, err := util.ParseRelativeFile(filename)
+	if err != nil {
+		return
+	}
+
+	// passports are separated by a blank line, so split the string by
+	// two consecutive newline characters
+	passportBlocks := strings.Split(*input, "\n\n")
+
+	// for each passport block, parse its values into a map of
+	// passport fields
+	exp := regexp.MustCompile(`\s`)
+	passports := []map[string]string{}
+	for _, block := range passportBlocks {
+		fields := exp.Split(block, -1)
+		passport := make(map[string]string)
+		for _, field := range fields {
+			fieldVals := strings.Split(field, ":")
+			passport[fieldVals[0]] = fieldVals[1]
+		}
+		passports = append(passports, passport)
+	}
+
+	// validate the passport
+	validPassports := []map[string]string{}
+	for _, passport := range passports {
+
+		// Potential Fields:
+		//
+		// byr (Birth Year)
+		// iyr (Issue Year)
+		// eyr (Expiration Year)
+		// hgt (Height)
+		// hcl (Hair Color)
+		// ecl (Eye Color)
+		// pid (Passport ID)
+		// cid (Country ID)
+
+		_, hasByr := passport["byr"]
+		_, hasIyr := passport["iyr"]
+		_, hasEyr := passport["eyr"]
+		_, hasHgt := passport["hgt"]
+		_, hasHcl := passport["hcl"]
+		_, hasEcl := passport["ecl"]
+		_, hasPid := passport["pid"]
+
+		if hasByr && hasIyr && hasEyr && hasHgt && hasHcl && hasEcl && hasPid {
+			validPassports = append(validPassports, passport)
+		}
+	}
+
+	fmt.Println(len(validPassports))
+}
---
+++ 04/README.md
@@ -0,0 +1,17 @@
+# Advent of Code Day 4
+
+## First Problem
+
+Get the answer to the first problem by running this command:
+
+```bash
+go run 01/04-1.go ./relative/path/to/file.txt
+```
+
+## Second Problem
+
+Get the answer to the first problem by running this command:
+
+```bash
+go run 02/04-2.go ./relative/path/to/file.txt
+```
